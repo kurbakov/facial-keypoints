5bf31fb847bec518a9189c046879b873fb54ea32
kurbakov <kurbakov.d@gmail.com>
2019-04-14 22:18:31 +0200

    extend the trainer file functionality

--- trainer.py
+++ trainer.py
@@ -5,11 +5,11 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-from data_load.data_set import FacialKeypointsDataset
-from data_load.rescale import Rescale
-from data_load.random_crop import RandomCrop
-from data_load.normalize import Normalize
-from data_load.to_tensor import ToTensor
+from utils.data_set import FacialKeypointsDataset
+from utils.rescale import Rescale
+from utils.random_crop import RandomCrop
+from utils.normalize import Normalize
+from utils.to_tensor import ToTensor
 
 from models.kurbakov_net import KurbakovNet
 from models.naimish_net import NaimishNet
@@ -70,22 +70,43 @@ def train_net(neural_net, n_epochs, criterion, train_loader, optimizer_type, lr)
     print('Finished Training')
 
 
-data_transform = transforms.Compose([Rescale(250), RandomCrop(224), Normalize(), ToTensor()])
-transformed_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keypoints.csv',
-                                             root_dir='data/training/',
-                                             transform=data_transform)
+def test_multiple_setups(n_epochs, batches, optimizer_types, learning_rates):
+    data_transform = transforms.Compose([Rescale(250), RandomCrop(224), Normalize(), ToTensor()])
+    transformed_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keypoints.csv',
+                                                 root_dir='data/training/',
+                                                 transform=data_transform)
+
+    # We use a for loop just to test and see how the variables influence the model accurac
+    for batch in batches:
+        data_loader = DataLoader(transformed_dataset, batch_size=batch, shuffle=True, num_workers=4)
+        for optimizer in optimizer_types:
+            for lr in learning_rates:
+                print("Batch:", batch)
+                print("Optimizer:", optimizer)
+                print("lr:", lr)
+                print("NNet:", "KurbakovNet")
+                train_net(KurbakovNet(), n_epochs, nn.MSELoss(), data_loader, optimizer, lr)
+                print("NNet:", "NaimishNet")
+                train_net(NaimishNet(), n_epochs, nn.MSELoss(), data_loader, optimizer, lr)
 
 
-n_epochs = 1
-# We use a for loop just to test and see how the variables influence the model accurac
-for batch in [10, 20, 30]:
-    data_loader = DataLoader(transformed_dataset, batch_size=batch, shuffle=True, num_workers=4)
-    for optimizer in ["Adam", "SGD", "RMS"]:
-        for lr in [0.001, 0.005, 0.01]:
-            print("Batch:", batch)
-            print("Optimizer:", optimizer)
-            print("lr:", lr)
-            print("NNet:", "KurbakovNet")
-            train_net(KurbakovNet(), n_epochs, nn.MSELoss(), data_loader, optimizer, lr)
-            print("NNet:", "NaimishNet")
-            train_net(NaimishNet(), n_epochs, nn.MSELoss(), data_loader, optimizer, lr)
+def train_and_save(n_epoch, batch_size, optimizer, learning_rate, path):
+    data_transform = transforms.Compose([Rescale(250), RandomCrop(224), Normalize(), ToTensor()])
+    transformed_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keypoints.csv',
+                                                 root_dir='data/training/',
+                                                 transform=data_transform)
+
+    data_loader = DataLoader(transformed_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    net = KurbakovNet()
+    train_net(net, n_epoch, nn.MSELoss(), data_loader, optimizer, learning_rate)
+
+    torch.save(net.state_dict(), path)
+
+
+if __name__ == '__main__':
+    # try different combinations
+    test_multiple_setups(1, [8, 16, 32, 64], ["Adam", "SGD", "RMS"], [0.001, 0.005, 0.01])
+
+    # Once you know which model is the best, train it and save
+    neural_net = KurbakovNet()
+    train_and_save(1, 64, optim.SGD(params=neural_net.parameters(), lr=0.1), "./saved_models/my_model.pt")
